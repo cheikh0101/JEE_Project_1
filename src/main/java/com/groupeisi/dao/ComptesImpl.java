85a84143a6f23b21d41e2dcb31f7b146d144c71f
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2023-01-23 15:37:43 +0000

    ajout des captures des implementations des interfaces

--- src/main/java/com/groupeisi/dao/ComptesImpl.java
+++ src/main/java/com/groupeisi/dao/ComptesImpl.java
@@ -1,5 +1,81 @@
 package com.groupeisi.dao;
 
-public class ComptesImpl {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.groupeisi.entities.Comptes;
+
+
+public class ComptesImpl implements IComptes {
+	
+	private EntityManager em;
+	
+	public ComptesImpl() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("suiviStockPU");
+		em = emf.createEntityManager();
+	}
+
+	@Override
+	public int add(Comptes compte) {
+		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			em.persist(compte);
+			em.getTransaction().commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	@Override
+	public List<Comptes> getAll() {
+		// TODO Auto-generated method stub
+		try {
+			return em.createQuery("SELECT p FROM Compte p").getResultList();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public int update(Comptes compte) {
+		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			em.merge(compte);
+			em.getTransaction().commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	@Override
+	public int delete(String ref) {
+		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			em.remove(em.find(Comptes.class, ref));
+			em.getTransaction().commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	@Override
+	public Comptes get(String ref) {
+		// TODO Auto-generated method stub
+		return em.find(Comptes.class, ref);
+	}
 
 }
--- src/main/java/com/groupeisi/dao/DroitsImpl.java
+++ src/main/java/com/groupeisi/dao/DroitsImpl.java
@@ -1,5 +1,81 @@
 package com.groupeisi.dao;
 
-public class DroitsImpl {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.groupeisi.entities.Comptes;
+import com.groupeisi.entities.Droits;
+
+public class DroitsImpl implements IDroits {
+
+	private EntityManager em;
+	
+	public DroitsImpl() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("suiviStockPU");
+		em = emf.createEntityManager();
+	}
+
+	@Override
+	public int add(Droits droit) {
+		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			em.persist(droit);
+			em.getTransaction().commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	@Override
+	public List<Droits> getAll() {
+		// TODO Auto-generated method stub
+		try {
+			return em.createQuery("SELECT p FROM Droits p").getResultList();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public int update(Droits droit) {
+		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			em.merge(droit);
+			em.getTransaction().commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	@Override
+	public int delete(String ref) {
+		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			em.remove(em.find(Droits.class, ref));
+			em.getTransaction().commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	@Override
+	public Droits get(String ref) {
+		// TODO Auto-generated method stub
+		return em.find(Droits.class, ref);
+	}
 
 }
