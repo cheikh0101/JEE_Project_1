2d7c25a1f13f64f330d110389684ca02b2b084ae
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2023-01-23 15:00:42 +0000

    ajout des captures des 2 interfaces

--- src/main/java/com/groupeisi/dao/IComptes.java
+++ src/main/java/com/groupeisi/dao/IComptes.java
@@ -1,5 +1,14 @@
 package com.groupeisi.dao;
 
+import java.util.List;
+
+import com.groupeisi.entities.Comptes;
+
 public interface IComptes {
 
+	public int add(Comptes compte);
+	public List<Comptes> getAll();
+	public int update(Comptes compte);
+	public int delete(String ref);
+	public Comptes get(String ref);
 }
--- src/main/java/com/groupeisi/dao/IDroits.java
+++ src/main/java/com/groupeisi/dao/IDroits.java
@@ -1,5 +1,14 @@
 package com.groupeisi.dao;
 
+import java.util.List;
+
+import com.groupeisi.entities.Droits;
+
 public interface IDroits {
 
+	public int add(Droits droit);
+	public List<Droits> getAll();
+	public int update(Droits droit);
+	public int delete(String ref);
+	public Droits get(String ref);
 }
